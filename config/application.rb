19aedefcb235a973d42da025661742ee9f712267
Oleksandr Kychun <kichun.oleksandr@gmail.com>
2017-07-15 23:42:55 +0300

    capistrano setup: load env variables

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,15 @@ Bundler.require(*Rails.groups)
 
 module RubyRogues
   class Application < Rails::Application
+    config.before_configuration do
+      env_file = Rails.root.join("config", 'config.yml').to_s
+      if File.exists?(env_file)
+        YAML.load_file(env_file).each do |key, value|
+          ENV[key.to_s] = value
+        end
+      end
+    end
+
     config.generators do |g|
       g.test_framework :rspec,
         fixtures: true,
