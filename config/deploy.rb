c1fa0a948c397ccbb50a85c5dcf9eed2181d45b3
Oleksandr Kychun <kichun.oleksandr@gmail.com>
2017-07-16 20:57:53 +0300

    removed bin directory from linked

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,7 @@ set :deploy_to, "/var/www/ruby-rogues"
 set :linked_files, %w{config/database.yml config/config.yml}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 
 set :ssh_options, forward_agent: true
 
