fd32fde54352619009522db7aebce3222cc7f202
Ivan Sheremeta <iisheremeta@gmail.com>
2017-07-16 19:41:15 +0300

    added picture model validation

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,12 @@
 class Picture < ApplicationRecord
   mount_uploader :picture, PictureUploader
   belongs_to :imageable, polymorphic: true
+
+  validates_processing_of :picture
+  validate :picture_size_validation
+
+  private
+    def picture_size_validation
+      errors[:picture] << "should be less than 500KB" if picture.size > 0.5.megabytes
+    end
 end
