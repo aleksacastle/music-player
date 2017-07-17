d2159afed4a520b63a5c2e1aacaad3f7dddbee83
aleksacastle <aaklochko@gmail.com>
2017-07-17 12:51:29 +0300

    registration controller||rubocop a

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,17 @@
+class RegistrationsController < Devise::RegistrationsController
+  ARTIST_ROLE = 1
+  USER_ROLE = 0
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+      .merge(role)
+  end
+
+  def role
+    return { role: ARTIST_ROLE } if params.fetch(:user, {})
+      .fetch(:artist, USER_ROLE) == ARTIST_ROLE.to_s
+    { role: USER_ROLE }
+  end
+end
