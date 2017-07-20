442bd64ef5f92c254ed123d182ff597aa20d200a
aleksacastle <aaklochko@gmail.com>
2017-07-21 02:19:21 +0300

    playlist validation errors

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -16,4 +16,6 @@ class Playlist < ApplicationRecord
   belongs_to :user
   has_one :picture, as: :imageable
   accepts_nested_attributes_for :picture
+
+  validates :title, :file_cover, presence: true
 end
