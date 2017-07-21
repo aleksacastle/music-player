970017d7ee7167ea68ff3e1c9b789dcfa684b1ba
oleksandr_kychun <kichun.oleksandr@gmail.com>
2017-07-21 16:47:28 +0000

    breadcrumbs playlist_songs

--- app/controllers/playlist_songs_controller.rb
+++ app/controllers/playlist_songs_controller.rb
@@ -6,6 +6,8 @@ class PlaylistSongsController < BaseController
   end
 
   def create
+    # form object, there you will validate :useless_songs_ids, :new_songs_ids
+    # service object: that will complete the same work
     @playlist.songs.delete(get_songs(useless_songs_ids)) if useless_songs_ids
     @playlist.songs << get_songs(new_songs_ids)          if new_songs_ids
 
@@ -17,19 +19,20 @@ class PlaylistSongsController < BaseController
     def set_playlist
       @playlist ||= Playlist.find(params[:id])
     end
-
+    # posible to use query object
+    # move it inside service
     def get_songs(ids)
       Song.where(id: ids)
     end
-
+    # move this helper inside service
     def playlist_songs_ids
       @playlist_songs_ids ||= @playlist.songs.pluck(:id)
     end
-
+    # move this helper inside service
     def new_songs_ids
       params[:songs] - playlist_songs_ids if params[:songs]
     end
-
+    # move this helper inside service
     def useless_songs_ids
       return playlist_songs_ids if params[:songs].blank?
 
