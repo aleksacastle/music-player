b5190c7eddffdff334955b010c5c83b3480971ad
Igor Viblyy <igor.viblyy@gmail.com>
2017-07-26 17:56:23 +0300

    add dashboard to admin

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,33 +1,54 @@
 ActiveAdmin.register_page "Dashboard" do
 
-  menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
+  menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
-  content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
-    end
+  content title: proc { I18n.t("active_admin.dashboard") } do
+    # div class: "blank_slate_container", id: "dashboard_default_message" do
+    #   span class: "blank_slate" do
+    #     span I18n.t("active_admin.dashboard_welcome.welcome")
+    #     small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    #   end
+    # end
 
     # Here is an example of a simple dashboard with columns and panels.
     #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    columns do
+      column do
+        panel "Recent Users" do
+          ul do
+            # @users = User.all
+            User.all.map do |user|
+              li link_to(user.email, admin_user_path(user))
+            end
+          end
+        end
+      end
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
+      column do
+        panel "Info" do
+          para "Welcome to ActiveAdmin."
+        end
+      end
+
+      column do
+        panel "Recent Songs" do
+          ul do
+            Song.all.map do |song|
+              li link_to(song.title, admin_song_path(song))
+            end
+          end
+        end
+      end
+
+      column do
+        panel "Recent Playlist" do
+          ul do
+            Playlist.all.map do |playlist|
+              li link_to(playlist.title, admin_playlist_path(playlist))
+            end
+          end
+        end
+      end
+    end
   end # content
 end
