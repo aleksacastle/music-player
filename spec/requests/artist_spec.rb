205a7bf4dda4a44afe85ac0b6007583a9b691178
Denys Makarenko <makarenko.denys@gmail.com>
2017-07-30 17:24:37 +0300

    Move spec methods to helper

--- spec/requests/artist_spec.rb
+++ spec/requests/artist_spec.rb
@@ -1,14 +1,10 @@
 require 'rails_helper'
+include RequestsHelper
 
 describe 'Artist' do
 
   it 'has been created with relevant role' do
-    visit '/users/sign_up'
-    fill_in 'Email', with: 'admin@auto.ua'
-    fill_in 'Password', with: 'admin@auto.ua'
-    fill_in 'Password confirmation', with: 'admin@auto.ua'
-    page.check('user_artist')
-    click_on 'Sign Up'
+    manual_sign_up
     expect(page).to have_content('Welcome! You have signed up successfully.')
     click_on 'Log out'
     expect(User.first.role).to eq('artist')
@@ -17,31 +13,16 @@ describe 'Artist' do
 
   it 'is able to create new album' do
     artist = FactoryGirl.create(:user,role: 1)
-    visit '/users/sign_in'
-    fill_in 'Email', with: artist.email
-    fill_in 'Password', with: artist.password
-    click_button 'Log in'
-    visit albums_path
-    click_on 'Create album'
-    fill_in 'Title', with: 'album-test'
-    page.attach_file('album_picture_attributes_file', Rails.root + 'public/uploads/picture/file/1/mypic.jpg')
-    click_button 'Create Album'
+    manual_sign_in(artist)
+    create_album
     expect(page).to have_content('Album was successfully created.')
   end
 
   it 'is able to add new song to playlist' do
     artist = FactoryGirl.create(:user,role: 1)
     playlist = FactoryGirl.create(:playlist, user_id: artist.id)
-    visit '/users/sign_in'
-    fill_in 'Email', with: artist.email
-    fill_in 'Password', with: artist.password
-    click_button 'Log in'
-    visit '/playlists'
-    click_link 'Show'
-    click_link 'Add song'
-    fill_in 'Artist', with: 'artist-test'
-    fill_in 'Title', with: 'title-test'
-    click_on 'Create Song'
+    manual_sign_in(artist)
+    create_song
     expect(page).to have_content('Song was successfully created.')
   end
 end
