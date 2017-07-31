e8f1c67ce1ad70b41969a6b06f4dac35bdce0e7f
Igor Viblyy <igor.viblyy@gmail.com>
2017-07-31 16:04:26 +0300

    add cap production db:seed

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,3 +46,16 @@ set :rbenv_ruby, "2.4.1"
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all
+
+# Add this in config/deploy.rb
+# and run 'cap production deploy:seed' to seed your database
+desc "Runs rake db:seed"
+task seed: [:set_rails_env] do
+  on primary fetch(:migration_role) do
+    within release_path do
+      with rails_env: fetch(:rails_env) do
+        execute :rake, "db:seed"
+      end
+    end
+  end
+end
